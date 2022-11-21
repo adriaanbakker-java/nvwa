5a517de37ec9fe59407f215c49c0699295645e02
Adriaan Bakker <adriaan.bakker@hotmail.com>
2022-11-21 16:20:27 +0100

    niet verder door ontbreken schemas bij ontrafelen commodities

--- ontwikkel_nvwa/create_adriaan_log.sql
+++ ontwikkel_nvwa/create_adriaan_log.sql
@@ -1,21 +1,21 @@
 
-CREATE TABLE adriaan_log3 (
+CREATE TABLE adriaan_log4 (
   ID           NUMBER(10)    NOT NULL,
   logmsg  VARCHAR2(50)  NOT NULL);
 
 
-ALTER TABLE adriaan_log3 ADD (
-  CONSTRAINT pk_adriaan_log3 PRIMARY KEY (ID));
+ALTER TABLE adriaan_log4 ADD (
+  CONSTRAINT pk_adriaan_log4 PRIMARY KEY (ID));
 
-CREATE SEQUENCE seq_adriaan_log3 START WITH 1000;
+CREATE SEQUENCE seq_adriaan_log4 START WITH 1000;
 
 
-CREATE OR REPLACE TRIGGER adriaan_log3_bir
-BEFORE INSERT ON adriaan_log3
+CREATE OR REPLACE TRIGGER adriaan_log4_bir
+BEFORE INSERT ON adriaan_log4
 FOR EACH ROW
 
 BEGIN
-  SELECT seq_adriaan_log3.NEXTVAL
+  SELECT seq_adriaan_log4.NEXTVAL
   INTO   :new.id
   FROM   dual;
 END;
