c3030671ba6ed1328a3f0e3c7d9949dda44830f6
Adriaan Bakker <adriaan.bakker@hotmail.com>
2022-11-24 14:46:10 +0100

    gncodes nu filteren en verwerken

--- ontwikkel_nvwa/create_adriaan_log.sql
+++ ontwikkel_nvwa/create_adriaan_log.sql
@@ -1,21 +1,22 @@
 
-CREATE TABLE adriaan_log4 (
+CREATE TABLE adriaan_log (
   ID           NUMBER(10)    NOT NULL,
-  logmsg  VARCHAR2(50)  NOT NULL);
+  logmsg  VARCHAR2(4000)  NOT NULL,
+  clobwaarde);
 
 
-ALTER TABLE adriaan_log4 ADD (
-  CONSTRAINT pk_adriaan_log4 PRIMARY KEY (ID));
+ALTER TABLE adriaan_log ADD (
+  CONSTRAINT pk_adriaan_log PRIMARY KEY (ID));
 
-CREATE SEQUENCE seq_adriaan_log4 START WITH 1000;
+CREATE SEQUENCE seq_adriaan_log START WITH 1000;
 
 
-CREATE OR REPLACE TRIGGER adriaan_log4_bir
-BEFORE INSERT ON adriaan_log4
+CREATE OR REPLACE TRIGGER adriaan_log_bir
+BEFORE INSERT ON adriaan_log
 FOR EACH ROW
 
 BEGIN
-  SELECT seq_adriaan_log4.NEXTVAL
+  SELECT seq_adriaan_log.NEXTVAL
   INTO   :new.id
   FROM   dual;
 END;
